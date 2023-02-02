0a378b4918ab2095da0bd552a5daa824e9fb6d05
w660i <nikolajromanovic@gmail.com>
2023-02-02 23:44:50 +0300

    start_files

---
+++ Nick_2021/nick_2021/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def about(request):
+    a = 5+6
+    return render(request,'about.html',{'gretting':a})
+
+def home(request):
+    return render(request,'home.html')
+
+def reverse(request):
+    return render(request,'reverse.html')
